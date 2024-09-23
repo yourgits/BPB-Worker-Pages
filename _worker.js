6d5c5b474a0744f73c7fd036dc0731d1193befba
yourgits <project20010@proton.me>
2024-09-24 00:47:55 +0800

    Update _worker.js
    修改默认uuid

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'e60921f0-c492-484e-9110-7d189153729d';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
